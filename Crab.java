252e71dd09de14868b185cfe4b7bd7bf4345d30d
AsherWhi <89489616+AsherWhi@users.noreply.github.com>
2021-08-31 15:06:22 -0700

    Finished Part 2 and 3

--- Crab.java
+++ Crab.java
@@ -1,14 +1,54 @@
 import greenfoot.*;
 
 /**
- * This class defines a crab. Crabs live on the beach.
- */
+ * This class controls the Crab object
+ * @author: Asher White
+ * @version 8/30/2021
+*/
 public class Crab extends Actor
 {
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
     }
-}
 
+    // Moves the Crab
+    
+    // Turns the crab
+    private void turnAtEdge()
+    {
+        if (isAtEdge())
+        {
+            turn(-50);
+        }
+    }
+    // Checks for user key presses so user can turn the Crab
+    private void checkKeyPress()
+    {
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(4);
+        }
+        if(Greenfoot.isKeyDown("left"))
+        {
+            turn(-4);
+        }
+        if(Greenfoot.isKeyDown("up"))
+        {
+            Greenfoot.delay(60);
+        }  
+        // Checks for collisions with other objects
+    }
+    private void onCollision()
+     {
+        if(isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+            Greenfoot.playSound("slurp.wav");
+        }
+    }
+    }
 
